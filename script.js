095f278181161d73ebaaf02802cf87dcc58675f0
Nilperalta <nildalisset.ll@gmail.com>
2026-06-05 18:17:13 -0500

    arreglando espacios

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const btnHeaderColor = document.getElementById('toggle-color');
 const colors = document.querySelector('.colors')
 const btnHeaderTalla = document.getElementById('toggle-talla');
 const talla = document.querySelector('.tallas');
+const tallaGuia = document.querySelector('.tallas__guia');
 const iconColor = document.querySelector('#toggle-color i');
 const iconTalla = document.querySelector('#toggle-talla i');
 
@@ -16,6 +17,7 @@ btnHeaderColor.addEventListener('click', () => {
 
 btnHeaderTalla.addEventListener('click',()=>{
     talla.classList.toggle('tallas--visible')
+    tallaGuia.classList.toggle('tallas__guia--visible')
     iconTalla.classList.toggle('fa-plus')
     iconTalla.classList.toggle('fa-minus')
 })
